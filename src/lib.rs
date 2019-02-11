c6dae6cd4a27b612325803405560d007bb9a20b7
tehAnswer <sergiorodriguezgijon@gmail.com>
2019-02-12 00:18:36 +0100

    Added scaffolded project files

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+extern crate reqwest;
+#[cfg(test)]
+extern crate mockito;
+
+
+pub mod operations;
+pub mod models;
